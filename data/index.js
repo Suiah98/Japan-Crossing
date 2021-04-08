7a639d8e37d8611e8c1d126d2782b7fadb264d81
Suian98 <ianbbpp@gmail.com>
2021-04-08 20:18:16 +0200

    fix time

--- data/index.js
+++ data/index.js
@@ -67,13 +67,65 @@ function JapanTime() {
 }
 JapanTime();
 
+let sptime = nd.toLocaleString();
+sptime = sptime.split(':')
+
+
+//console.log(sptime[0])
+
 function acnhmusicon() {
   document.getElementById("acnhmusicon").style.display = 'none';
-  if (/4:*/g.test(nd)) {
+  if (/0::*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/12am.mp3');
+    audio.play();
+    console.log("0am");
+  } else if (/1:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/1am.mp3');
+    audio.play();
+    console.log("1am");
+  } else if (/2:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/2am.mp3');
+    audio.play();
+    console.log("2am");
+  } else if (/3:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/3am.mp3');
+    audio.play();
+    console.log("3am");
+  } else if (/4:*$/g.test(sptime[0])) {
     var audio = new Audio('data/music/4am.mp3');
     audio.play();
-  } else if (/5:*/g.test(nd)) {
+    console.log("4am");
+  } else if (/5:*$/g.test(sptime[0])) {
     var audio = new Audio('data/music/5am.mp3');
     audio.play();
+    console.log("5am");
+  } else if (/6:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/6am.mp3');
+    audio.play();
+    console.log("6am");
+  } else if (/7:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/7am.mp3');
+    audio.play();
+    console.log("7am");
+  } else if (/8:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/8am.mp3');
+    audio.play();
+    console.log("8am");
+  } else if (/9:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/9am.mp3');
+    audio.play();
+    console.log("9am");
+  } else if (/10:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/10am.mp3');
+    audio.play();
+    console.log("10am");
+  } else if (/11:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/11am.mp3');
+    audio.play();
+    console.log("11am");
+  } else if (/12:*$/g.test(sptime[0])) {
+    var audio = new Audio('data/music/12pm.mp3');
+    audio.play();
+    console.log("12pm");
   }
 }

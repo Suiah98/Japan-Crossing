5a857a357ed887d688f6ac5c34c39dd958a03b33
Suian <ianbbpp@gmail.com>
2023-12-30 21:19:02 +0100

    cams updated

--- debug.js
+++ debug.js
@@ -1,8 +1,8 @@
 let videoIds = [
   'n3B8fp-Henc', 'Z5tqB0y3pB8', 'Fb0imHesPEk', 'gFRtAAmiFbE',
-  'SDRS6JQulmI', 'jag75WGaI9s', 'D7MJsCTv2zg', 'Lybr9mw41ic',
+  'CF1vS8DdBIk', 'c7BXfK2OCfI', 'pQeiX4MIjm4', 'Lybr9mw41ic',
   'lA6TaaMGgDo', '_kNCRJ2Ray0', 'qYnCG4J26d8', 'DjdUEyjx8GM',
-  'psdm2pTs4wQ', '8i9ltzhq80s', 'GEPJYPznC_Q', 'Bj6A3LFL9TM',
+  '2JcOHCCV4MA', '8i9ltzhq80s', 'GEPJYPznC_Q', 'Bj6A3LFL9TM',
   '6bPfX9m7wVU', 'VhGxxZ1X9dw', '3kPH7kTphnE', 'LR7i5_pkJBg'
 ];
   
